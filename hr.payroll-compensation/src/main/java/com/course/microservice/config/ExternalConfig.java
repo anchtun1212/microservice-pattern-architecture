fffb0bd46713f60a49ff4d3d678b4ae179c4a093
anchtun1212 <med.aymen.charrada@gmail.com>
2023-10-05 09:58:25 +0300

    add hr.payroll-compensation microservice

---
+++ hr.payroll-compensation/src/main/java/com/course/microservice/config/ExternalConfig.java
@@ -0,0 +1,7 @@
+package com.course.microservice.config;
+
+//@Configuration
+//@PropertySources({ @PropertySource(factory = YamlPropertySourceFactory.class, value = { "classpath:microservice.yml",
+//		"file:/conf/microservice.yml", "file:c:/conf/microservice.yml" }, ignoreResourceNotFound = true) })
+public class ExternalConfig {
+}
