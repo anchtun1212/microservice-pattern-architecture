dd9b7c9ae6fed85c4b3c98e43a9a32eadc77ad6f
anchtun1212 <med.aymen.charrada@gmail.com>
2023-10-05 11:49:05 +0300

    add log folder to git ignore

--- microservice-chassis/src/main/java/com/course/microservice/config/ExternalConfig.java
+++ microservice-chassis/src/main/java/com/course/microservice/config/ExternalConfig.java
@@ -1,11 +1,7 @@
 package com.course.microservice.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-
-@Configuration
-@PropertySources({ @PropertySource(factory = YamlPropertySourceFactory.class, value = { "classpath:microservice.yml",
-		"file:/conf/microservice.yml", "file:c:/conf/microservice.yml" }, ignoreResourceNotFound = true) })
+//@Configuration
+//@PropertySources({ @PropertySource(factory = YamlPropertySourceFactory.class, value = { "classpath:microservice.yml",
+//		"file:/conf/microservice.yml", "file:c:/conf/microservice.yml" }, ignoreResourceNotFound = true) })
 public class ExternalConfig {
 }
