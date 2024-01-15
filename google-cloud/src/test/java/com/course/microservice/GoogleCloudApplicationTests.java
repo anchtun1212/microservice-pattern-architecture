48b5b91014348b9bfe83df39d9c119bade8d36db
anchtun1212 <med.aymen.charrada@gmail.com>
2024-01-15 11:50:56 +0300

    google-cloud: first commit

---
+++ google-cloud/src/test/java/com/course/microservice/GoogleCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
